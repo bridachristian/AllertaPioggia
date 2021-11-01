ba4052a6994c18e0f7fb00cf55c528c42570d88b
Christian Brida <bridachristian@gmail.com>
2021-11-01 16:39:52 +0100

    das

--- influx_write.R
+++ influx_write.R
@@ -3,7 +3,7 @@ options(scipen=999)
 
 library(dplyr)
 # library(stringr)
-library(influxdbr)
+# library(influxdbr)
 # library(xts)
 
 
@@ -16,17 +16,22 @@ data = read.csv(file_csv, stringsAsFactors = F,sep = ";",dec = ",",row.names = N
 data$data = as.POSIXct(data$data, format = "%d/%m/%Y %H:%M", tz= "Etc/GMT-1")
 
 
-# data = data %>% mutate(datetime_ns = as.numeric(data)*1000000000)
-# line = c()
-# 
-# data = data %>%
-#   mutate(line = paste("pioggia", " value=", pioggia, " ",datetime_ns, sep=""))
-# 
-# line = c(all_new$line)
+data = data %>% mutate(datetime_ns = as.numeric(data)*1000000000)
+line = c()
 
+data = data %>%
+  mutate(line = paste("pioggia", " value=", pioggia, " ",datetime_ns, sep=""))
 
-influx_write(con = con, 
-             db = "piogggia",
-             x = data,
-             time_col = "data",
-             measurement = "pioggia")
+line = c(data$line)
+
+filename = paste("./Dati/Realtime/dati_lastweek.txt",sep = "")
+output.file <- file(filename, "wb") 
+
+write("# DDL", file = output.file)
+write("",file = output.file, append = TRUE)
+write("# DML", file = output.file, append = TRUE)
+write("# CONTEXT-DATABASE: pioggia", file = output.file, append = TRUE)
+write("",file = output.file, append = TRUE)
+write.table(x = line, file = output.file,
+            quote = FALSE,row.names = FALSE, col.names = FALSE,append = TRUE)
+close(output.file)
